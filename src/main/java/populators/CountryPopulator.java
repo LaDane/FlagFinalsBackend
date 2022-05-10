8f9b91a5bf1e1fc6476fba2266a372471dc50754
JonasRex <78900583+JonasRex@users.noreply.github.com>
2022-05-10 16:01:23 +0200

    changed country svg

--- src/main/java/populators/CountryPopulator.java
+++ src/main/java/populators/CountryPopulator.java
@@ -23,27 +23,27 @@ public class CountryPopulator {
         Continent europe = ContinentFacade.getFacade(emf).getByName("Europe");
         CountryFacade facade = CountryFacade.getFacade(emf);
 
-        Country germany = new Country("Germany", europe, facade.getSVG("germany"));
-        Country gb = new Country("Great Britain", europe, facade.getSVG("gb"));
-        Country france = new Country("France", europe, facade.getSVG("france"));
-        Country italy = new Country("Italy", europe, facade.getSVG("italy"));
-        Country spain = new Country("Spain", europe, facade.getSVG("spain"));
-        Country ukraine = new Country("Ukraine", europe, facade.getSVG("ukraine"));
-        Country russia = new Country("Russia", europe, facade.getSVG("russian%20federation"));
-        Country poland = new Country("Poland", europe, facade.getSVG("poland"));
-        Country romania = new Country("Romania", europe, facade.getSVG("romania"));
-        Country netherlands = new Country("Netherlands", europe, facade.getSVG("netherlands"));
-        Country belgium = new Country("Belgium", europe, facade.getSVG("belgium"));
-        Country greece = new Country("Greece", europe, facade.getSVG("greece"));
-        Country portugal = new Country("Portugal", europe, facade.getSVG("portugal"));
-        Country sweden = new Country("Sweden", europe, facade.getSVG("sweden"));
-        Country hungary = new Country("Hungary", europe, facade.getSVG("hungary"));
-        Country belarus = new Country("Belarus", europe, facade.getSVG("belarus"));
-        Country austria = new Country("Austria", europe, facade.getSVG("austria"));
-        Country serbia = new Country("Serbia", europe, facade.getSVG("serbia"));
-        Country switzerland = new Country("Switzerland", europe, facade.getSVG("switzerland"));
-        Country denmark = new Country("Denmark", europe, facade.getSVG("denmark"));
-        Country norway = new Country("Norway", europe, facade.getSVG("norway"));
+        Country germany = new Country("Germany", europe, "276");
+        Country gb = new Country("Great Britain", europe, "826");
+        Country france = new Country("France", europe, "250");
+        Country italy = new Country("Italy", europe, "380");
+        Country spain = new Country("Spain", europe, "724");
+        Country ukraine = new Country("Ukraine", europe, "804");
+        Country russia = new Country("Russia", europe, "643");
+        Country poland = new Country("Poland", europe, "616");
+        Country romania = new Country("Romania", europe, "642");
+        Country netherlands = new Country("Netherlands", europe, "528");
+        Country belgium = new Country("Belgium", europe, "056");
+        Country greece = new Country("Greece", europe, "300");
+        Country portugal = new Country("Portugal", europe, "620");
+        Country sweden = new Country("Sweden", europe, "752");
+        Country hungary = new Country("Hungary", europe, "348");
+        Country belarus = new Country("Belarus", europe, "112");
+        Country austria = new Country("Austria", europe, "040");
+        Country serbia = new Country("Serbia", europe, "688");
+        Country switzerland = new Country("Switzerland", europe, "756");
+        Country denmark = new Country("Denmark", europe, "208");
+        Country norway = new Country("Norway", europe, "578");
 
         try {
             em.getTransaction().begin();
